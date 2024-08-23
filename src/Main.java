daf166c2e636fb5cd9b3d7535911d32d02ab50ac
Rengifo Ramirez <erika.rengifo0102@gmail.com>
2024-08-23 09:48:31 -0500

    Se realiza la descripćión de la primera opción del menú principal (Datos Primitivos)

--- src/Main.java
+++ src/Main.java
@@ -49,13 +49,18 @@ public class Main {
                     opcdp = scanner.nextInt();
                     switch (opcdp) {
                         case 1:
-                            System.out.println("***************************************");
-                            System.out.println("*                  BYTE               *");
-                            System.out.println("***************************************");
-                            System.out.println("***************************************");
-                            System.out.println("*         1. Volver al menú anterior  *");
-                            System.out.println("*         0. Salir.                   *");
-                            System.out.println("***************************************");
+                            System.out.println("****************************************");
+                            System.out.println("*                  BYTE                *");
+                            System.out.println("* Representa un tipo de dato de 8 bits *");
+                            System.out.println("* Puede almacenar valores numéricos en *");
+                            System.out.println("* el rango de -128 a 127               *");
+                            System.out.println("*                                      *");
+                            System.out.println("*                EJEMPLO               *");
+                            System.out.println("*         byte diasMes = 31;           *");
+                            System.out.println("*                                      *");
+                            System.out.println("*      1. Volver al menú anterior      *");
+                            System.out.println("*      0. Salir.                       *");
+                            System.out.println("****************************************");
                             System.out.print("Digite su opción: ");
                             opcad = scanner.nextInt();
                             switch (opcad) {
@@ -69,13 +74,22 @@ public class Main {
                             }
                             break;
                         case 2:
-                            System.out.println("***************************************");
-                            System.out.println("*                SHORT                *");
-                            System.out.println("***************************************");
-                            System.out.println("***************************************");
-                            System.out.println("*         1. Volver al menú anterior  *");
-                            System.out.println("*         0. Salir.                   *");
-                            System.out.println("***************************************");
+                            System.out.println("****************************************");
+                            System.out.println("*                SHORT                 *");
+                            System.out.println("****************************************");
+                            System.out.println("* Este tipo de dato nos permiten       *");
+                            System.out.println("* rabajar con números enteros ya sean  *");
+                            System.out.println("* positivos o negativos. Puede         *");
+                            System.out.println("* almacenar  valores numéricos en el   *");
+                            System.out.println("* rango –32,768 to 32,767              *");
+                            System.out.println("*                                      *");
+                            System.out.println("*                EJEMPLO               *");
+                            System.out.println("*      short temperatura = -10         *");
+                            System.out.println("*                                      *");
+                            System.out.println("****************************************");
+                            System.out.println("*         1. Volver al menú anterior   *");
+                            System.out.println("*         0. Salir.                    *");
+                            System.out.println("****************************************");
                             System.out.print("Digite su opción: ");
                             opcad = scanner.nextInt();
                             switch (opcad) {
@@ -90,11 +104,11 @@ public class Main {
                             break;
                         case 3:
                             System.out.println("***************************************");
-                            System.out.println("*                  INT                *");
+                            System.out.println("*                  INT                 *");
                             System.out.println("***************************************");
                             System.out.println("*Representa una cantidad finita de    *");
-                            System.out.println("*numero enteros, puede contener hasta *");
-                            System.out.println("*tres cifras.                         *");
+                            System.out.println("*números enteros, puede contener hasta *");
+                            System.out.println("*tres cifras.                          *");
                             System.out.println("*                                     *");
                             System.out.println("*                EJEMPLO              *");
                             System.out.println("*             int edad=101            *");
@@ -116,13 +130,19 @@ public class Main {
                             }
                             break;
                         case 4:
-                            System.out.println("***************************************");
-                            System.out.println("*                 LONG                *");
-                            System.out.println("***************************************");
-                            System.out.println("***************************************");
-                            System.out.println("*         1. Volver al menú anterior  *");
-                            System.out.println("*         0. Salir.                   *");
-                            System.out.println("***************************************");
+                            System.out.println("*****************************************");
+                            System.out.println("*                 LONG                  *");
+                            System.out.println("*****************************************");
+                            System.out.println("* Se utiliza cuando se necesitan números*");
+                            System.out.println("* enteros muy grandes o cuando los      *");
+                            System.out.println("* números tienen un valor decimal       *");
+                            System.out.println("*                                       *");
+                            System.out.println("*               EJEMPLO                 *");
+                            System.out.println("*   Long velocidadLuz =  299792458,36   *");
+                            System.out.println("*                                       *");
+                            System.out.println("*         1. Volver al menú anterior    *");
+                            System.out.println("*         0. Salir.                     *");
+                            System.out.println("*****************************************");
                             System.out.print("Digite su opción: ");
                             opcad = scanner.nextInt();
                             switch (opcad) {
@@ -136,13 +156,22 @@ public class Main {
                             }
                             break;
                         case 5:
-                            System.out.println("***************************************");
-                            System.out.println("*                 FLOAT               *");
-                            System.out.println("***************************************");
-                            System.out.println("***************************************");
-                            System.out.println("*         1. Volver al menú anterior  *");
-                            System.out.println("*         0. Salir.                   *");
-                            System.out.println("***************************************");
+                            System.out.println("****************************************");
+                            System.out.println("*                 FLOAT                *");
+                            System.out.println("****************************************");
+                            System.out.println("* Este tipo de dato nos permiten       *");
+                            System.out.println("* trabajar con números los cuales      *");
+                            System.out.println("* posean punto decimal ya sean         *");
+                            System.out.println("* positivos o negativos.               *");
+                            System.out.println("* utiliza 32 bits                      *");
+                            System.out.println("*                                      *");
+                            System.out.println("*               EJEMPLO                *");
+                            System.out.println("*        float pi = 3.1415926535       *");
+                            System.out.println("*                                      *");
+                            System.out.println("****************************************");
+                            System.out.println("*         1. Volver al menú anterior   *");
+                            System.out.println("*         0. Salir.                    *");
+                            System.out.println("****************************************");
                             System.out.print("Digite su opción: ");
                             opcad = scanner.nextInt();
                             switch (opcad) {
@@ -156,13 +185,21 @@ public class Main {
                             }
                             break;
                         case 6:
-                            System.out.println("***************************************");
-                            System.out.println("*                 DOUBLE              *");
-                            System.out.println("***************************************");
-                            System.out.println("***************************************");
-                            System.out.println("*         1. Volver al menú anterior  *");
-                            System.out.println("*         0. Salir.                   *");
-                            System.out.println("***************************************");
+                            System.out.println("****************************************");
+                            System.out.println("*                 DOUBLE               *");
+                            System.out.println("****************************************");
+                            System.out.println("* Almacena números en coma flotante    *");
+                            System.out.println("* con doble precisión de 64 bits,      *");
+                            System.out.println("* lo que proporciona una mayor         *");
+                            System.out.println("* precisión que float.                 *");
+                            System.out.println("*                                      *");
+                            System.out.println("*              EJEMPLO                 *");
+                            System.out.println("*  double e = 2.718281828459045235360; *");
+                            System.out.println("*                                      *");
+                            System.out.println("****************************************");
+                            System.out.println("*         1. Volver al menú anterior   *");
+                            System.out.println("*         0. Salir.                    *");
+                            System.out.println("****************************************");
                             System.out.print("Digite su opción: ");
                             opcad = scanner.nextInt();
                             switch (opcad) {
@@ -175,14 +212,23 @@ public class Main {
                                     break;
                             }
                             break;
+
                         case 7:
-                            System.out.println("***************************************");
-                            System.out.println("*                  CHAT               *");
-                            System.out.println("***************************************");
-                            System.out.println("***************************************");
-                            System.out.println("*         1. Volver al menú anterior  *");
-                            System.out.println("*         0. Salir.                   *");
-                            System.out.println("***************************************");
+                            System.out.println("*****************************************");
+                            System.out.println("*                  CHAR                 *");
+                            System.out.println("*****************************************");
+                            System.out.println("* Este tipo de dato nos permite         *");
+                            System.out.println("* trabajar con caracteres alfanuméricos *");
+                            System.out.println("* almacena hasta 16 bits                *");
+                            System.out.println("*                                       *");
+                            System.out.println("*             EJEMPLO                   *");
+                            System.out.println("*         char letraA = 'a';            *");
+                            System.out.println("*      char letraANumerico = 61;        *");
+                            System.out.println("*                                       *");
+                            System.out.println("*****************************************");
+                            System.out.println("*         1. Volver al menú anterior    *");
+                            System.out.println("*         0. Salir.                     *");
+                            System.out.println("*****************************************");
                             System.out.print("Digite su opción: ");
                             opcad = scanner.nextInt();
                             switch (opcad) {
@@ -196,13 +242,21 @@ public class Main {
                             }
                             break;
                         case 8:
-                            System.out.println("***************************************");
-                            System.out.println("*                BOOLEAN              *");
-                            System.out.println("***************************************");
-                            System.out.println("***************************************");
-                            System.out.println("*         1. Volver al menú anterior  *");
-                            System.out.println("*         0. Salir.                   *");
-                            System.out.println("***************************************");
+                            System.out.println("****************************************");
+                            System.out.println("*                BOOLEAN               *");
+                            System.out.println("****************************************");
+                            System.out.println("* Este tipo de datos nos permitirá     *");
+                            System.out.println("* trabajar con valores lógicos,        *");
+                            System.out.println("* verdadero o falso.                   *");
+                            System.out.println("*                                      *");
+                            System.out.println("*               EJEMPLO                *");
+                            System.out.println("*       boolean verdadero = true;      *");
+                            System.out.println("*       boolean falso = false;         *");
+                            System.out.println("*                                      *");
+                            System.out.println("****************************************");
+                            System.out.println("*         1. Volver al menú anterior   *");
+                            System.out.println("*         0. Salir.                    *");
+                            System.out.println("****************************************");
                             System.out.print("Digite su opción: ");
                             opcad = scanner.nextInt();
                             switch (opcad) {
@@ -216,6 +270,7 @@ public class Main {
                             }
                             break;
                         case 9:
+
                             break;
                         case 0:
                             cerrar = true;
